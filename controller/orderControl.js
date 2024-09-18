eafd26b82ac1ec95ca863ca68a77719ed8cec7bb
Innocent866 <igoldima@gmail.com>
2024-09-18 08:56:14 -0700

    updated

--- controller/orderControl.js
+++ controller/orderControl.js
@@ -2,7 +2,7 @@ const ORDER = require('../Model/order');
 
 // create an order
 const order = async (req,res)=>{
-    req.body.user = req.user.userId
+    req.body.user = req.user
 
     const {orderItems,recipient,address} = req.body;
     if(orderItems && orderItems.length === 0){
